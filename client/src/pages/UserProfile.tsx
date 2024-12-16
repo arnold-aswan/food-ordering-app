4d74d5594823d1623c7123d33139fe5c5a1455e2
arnold-aswan <fredyaswan@gmail.com>
2024-12-16 11:13:03 +0300

    prefill user profile page with data from the backend

--- client/src/pages/UserProfile.tsx
+++ client/src/pages/UserProfile.tsx
@@ -1,12 +1,19 @@
-import { useUpdateUser } from "@/api/usersApi";
+import { useGetCurrentUser, useUpdateUser } from "@/api/usersApi";
 import UserProfileForm from "@/forms/user-profile-forms/UserProfileForm";
 
 const UserProfile = () => {
-	const { updateUser, isPending } = useUpdateUser();
+	const { updateUser, isPending: isUpdateLoading } = useUpdateUser();
+	const { currentUser, isPending: isGetLoading } = useGetCurrentUser();
+
+	if (isGetLoading) return <span>Loading ...</span>;
+
+	if (!currentUser) return <span>Unable to load user profile</span>;
+
 	return (
 		<UserProfileForm
+			currentUser={currentUser}
 			onSave={updateUser}
-			isLoading={isPending}
+			isLoading={isUpdateLoading}
 		/>
 	);
 };
