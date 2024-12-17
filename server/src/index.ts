783f71916b7fc405eda5901bfc7dafae34be5fb7
arnold-aswan <fredyaswan@gmail.com>
2024-12-17 16:31:44 +0300

    installed and configured cloudinary for image uploads

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,19 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import usersRoute from "./routes/usersRoute";
+import myRestaurantRoute from "./routes/myRestaurantRoute";
+import { v2 as cloudinary } from "cloudinary";
 
 mongoose
 	.connect(process.env.MONGODB_URI as string)
 	.then(() => console.log("Connected to DB"))
 	.catch((error) => console.error("Error connecting to DB:", error));
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 app.use(express.json());
@@ -18,6 +26,7 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/user", usersRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 const port = 7000;
 app.listen(port, () => {
