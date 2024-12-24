2cbbe2f6dd8e71767e01ac0e0bb29751dcd25887
arnold-aswan <fredyaswan@gmail.com>
2024-12-24 12:33:17 +0300

    my restaurant route and controller

--- server/src/controllers/myRestaurantController.ts
+++ server/src/controllers/myRestaurantController.ts
@@ -1,37 +1,62 @@
 import { Request, Response, NextFunction } from "express";
 import Restaurant from "../models/restaurant";
-import cloudinary from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import mongoose from "mongoose";
 
-const createMyRestaurant = async (req: Request, res: Response) => {
+const getMyRestaurant = async (req: Request, res: Response) => {
 	try {
-		const image = req.file as Express.Multer.File;
+		const restaurant = await Restaurant.findOne({ user: req.userId });
+		if (!restaurant) {
+			res.status(404).json({ message: "restaurant not found" });
+			return;
+		}
+		res.json(restaurant);
+	} catch (error) {
+		console.error("Full error details:", error);
+		res.status(500).json({ message: "Error getting restaurant" });
+	}
+};
 
-		const base64Image = Buffer.from(image.buffer).toString("base64");
-		const dataURI = `data:${image.mimetype},base64,${base64Image}`;
+const createMyRestaurant = async (
+	req: Request,
+	res: Response
+): Promise<void> => {
+	try {
+		const existingRestaurant = await Restaurant.findOne({ user: req.userId });
 
-		// Upload an image
-		const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
+		if (existingRestaurant) {
+			res.status(409).json({ message: "User restaurant already exists" });
+			return;
+		}
+
+		const imageUrl = await uploadImage(req.file as Express.Multer.File);
 
 		const restaurant = new Restaurant(req.body);
-		restaurant.imageUrl = uploadResponse.url;
+		restaurant.imageUrl = imageUrl;
 		restaurant.user = new mongoose.Types.ObjectId(req.userId);
 		restaurant.lastUpdated = new Date();
 		await restaurant.save();
 		res.status(201).send(restaurant);
 	} catch (error) {
 		console.error("Full error details:", error);
-
-		// Log specific error properties
-		if (error instanceof Error) {
-			console.error("Error name:", error.name);
-			console.error("Error message:", error.message);
-			console.error("Error stack:", error.stack);
-		}
 		res.status(500).json({ message: "Error creating restaurant" });
 	}
 };
 
+const uploadImage = async (file: Express.Multer.File) => {
+	if (!file) {
+		throw new Error("No file provided for upload.");
+	}
+
+	const image = file;
+	const base64Image = Buffer.from(image.buffer).toString("base64");
+	const dataURI = `data:${image.mimetype};base64,${base64Image}`;
+
+	const uploadResponse = await cloudinary.uploader.upload(dataURI);
+	return uploadResponse.url;
+};
+
 export default {
 	createMyRestaurant,
+	getMyRestaurant,
 };
