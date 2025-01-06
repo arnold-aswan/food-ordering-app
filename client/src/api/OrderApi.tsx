ab0b923f9c74af52ee6dedff1d26eb975981196a
arnold-aswan <fredyaswan@gmail.com>
2025-01-06 11:59:13 +0300

    added checkout api request function

---
+++ client/src/api/OrderApi.tsx
@@ -0,0 +1,67 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+import { headers } from "@/constants/data.ts";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+type CheckoutSessionRequest = {
+  cartItems: {
+    menuItemId: string;
+    name: string;
+    quantity: string;
+  }[];
+  deliveryDetails: {
+    email: string;
+    name: string;
+    addressLine1: string;
+    city: string;
+    country?: string;
+  };
+  restaurantId: string;
+};
+
+export const useCreateCheckoutSession = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const createCheckoutSessionRequest = async (
+    checkoutSessionRequest: CheckoutSessionRequest,
+  ) => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await axios.post(
+      `${API_BASE_URL}/api/order/checkout/create-checkout-session`,
+      checkoutSessionRequest,
+      {
+        headers: {
+          ...headers,
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+
+    if (response.status !== 200) {
+      throw new Error("Failed to create checkout session");
+    }
+    console.log(response.data);
+    return response.data;
+  };
+
+  const {
+    mutateAsync: createCheckoutSession,
+    isPending,
+    isError,
+    reset,
+  } = useMutation({ mutationFn: createCheckoutSessionRequest });
+
+  if (isError) {
+    toast.error(isError.toString());
+    reset();
+  }
+
+  return {
+    createCheckoutSession,
+    isPending,
+  };
+};
