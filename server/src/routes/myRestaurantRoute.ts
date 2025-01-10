077c6cb20f13589495e61dd12475cb673f33baea
arnold-aswan <fredyaswan@gmail.com>
2025-01-10 13:02:41 +0300

    added my orders and order status route to restaurant routes

--- server/src/routes/myRestaurantRoute.ts
+++ server/src/routes/myRestaurantRoute.ts
@@ -3,36 +3,52 @@ import multer from "multer";
 import myRestaurantController from "../controllers/myRestaurantController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
 import { validateMyRestaurantRequest } from "../middleware/validation";
+import orderController from "../controllers/orderController";
 
 const router = express.Router();
 
 const storage = multer.memoryStorage();
 const upload = multer({
-	storage: storage,
-	limits: {
-		fileSize: 5 * 1024 * 1024, // 5mb
-	},
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5mb
+  },
 });
 
 router.get("/", jwtCheck, jwtParse, myRestaurantController.getMyRestaurant);
 
 // /api/my/restaurant
+
+router.get(
+  "/orders",
+  jwtCheck,
+  jwtParse,
+  myRestaurantController.getMyRestaurantOrders,
+);
+
+router.patch(
+  "/order/:orderId/status",
+  jwtCheck,
+  jwtParse,
+  myRestaurantController.updateOrderStatus,
+);
+
 router.post(
-	"/",
-	upload.single("imageFile"),
-	validateMyRestaurantRequest,
-	jwtCheck,
-	jwtParse,
-	myRestaurantController.createMyRestaurant
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  myRestaurantController.createMyRestaurant,
 );
 
 router.put(
-	"/",
-	upload.single("imageFile"),
-	validateMyRestaurantRequest,
-	jwtCheck,
-	jwtParse,
-	myRestaurantController.updateMyRestaurant
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  myRestaurantController.updateMyRestaurant,
 );
 
 export default router;
